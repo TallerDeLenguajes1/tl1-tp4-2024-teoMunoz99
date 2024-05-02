737987b5b88ceddcb49c352e58d2965e5a453510
Tadeo Munoz <tadeomunoz09@hotmail.com>
2024-05-02 13:04:58 -0300

    arregle la funcion de generarid, no estaba verificando que el puntero que recibe la funcion no sea nulo

--- punto2.c
+++ punto2.c
@@ -55,43 +55,46 @@ void insertarUltimo(Nodo *inicio, Nodo *nuevoNodo) {
 
 int generarID(Nodo *inicio);
 int generarID(Nodo *inicio) {
+    if (inicio == NULL) {
+        // Si el inicio es NULL, no hay nodos en la lista, entonces el ID comienza en 1000
+        return 1000;
+    }
     Nodo *aux = inicio;
-    //verifico si el inicio apunta a null, si es asi es el primer nodo entonces su id empieza en mil
+    // Verifico si hay solo un nodo en la lista
     if (aux->Siguiente == NULL) {
         return 1000;
-    } else {// si entra en el else es porque ya existian nodos entonces leemos el id del ultimo y lo retornamos incrementado
+    } else {
+        // Buscar el último nodo en la lista
         while (aux->Siguiente != NULL) {
             aux = aux->Siguiente;
         }
-        // una vez que aux esta posicionado en el nodo final retorno su id incrementado en 1
-        return aux->T.TareaID++;
+        // El ID del nuevo nodo es el ID del último nodo + 1
+        return aux->T.TareaID + 1;
     }
 }
 
-
 Tarea crearTarea(Nodo *inicio);
 Tarea crearTarea(Nodo *inicio) {
-    char buff[50];//var aux para guardar temporalmente la descripcion
+    char buff[50];  // var aux para guardar temporalmente la descripcion
     Tarea tNueva;
     tNueva.TareaID = generarID(inicio);
     printf("\nIngrese la descripcion: ");
     fflush(stdin);
     gets(buff);
-    tNueva.Descripcion = (char*)malloc(sizeof(char)* strlen(buff)+1);
-    strcpy(tNueva.Descripcion,buff);
+    tNueva.Descripcion = (char *)malloc(sizeof(char) * strlen(buff) + 1);
+    strcpy(tNueva.Descripcion, buff);
     printf("\nIngrese la duracion: ");
     scanf("%d", &tNueva.Duracion);
     return tNueva;
 }
 
 void cargarTarea(Nodo *inicio);
-void cargarTarea(Nodo *inicio){
-    int opcion;//variable bandera para salir del bucle
-    do
-    {
+void cargarTarea(Nodo *inicio) {
+    int opcion;  // variable bandera para salir del bucle
+    do {
         Tarea tNueva = crearTarea(inicio);
         Nodo *nodoNuevo = CrearNodo(tNueva);
-        insertarUltimo(inicio,nodoNuevo);
+        insertarUltimo(inicio, nodoNuevo);
 
         printf("\nDesea cargar otra tarea? Ingrese 1 o 2");
         printf("\n1-SI");
@@ -100,8 +103,17 @@ void cargarTarea(Nodo *inicio){
     } while (opcion != 2);
 }
 
+void mostrarTareas(Nodo *inicio);
+void mostrarTareas(Nodo *inicio) {
+    Nodo *aux = inicio;
+    while (aux->Siguiente) {
+        printf("\n-Tarea ID: %d", aux->T.TareaID);
+    }
+}
+
 int main() {
     Nodo *tareasPendientes = CrearListaVacia();
     Nodo *tareasRealizadas = CrearListaVacia();
+    cargarTarea(tareasPendientes);
     return 0;
 }
