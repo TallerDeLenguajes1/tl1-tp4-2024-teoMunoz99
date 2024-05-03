5b37a8424ed23ff1126f3fb445ea94c64430a4ba
Tadeo Munoz <tadeomunoz09@hotmail.com>
2024-05-02 23:40:37 -0300

    agregue la funcion de crear lista vacia

--- punto2.c
+++ punto2.c
@@ -20,114 +20,12 @@ Nodo *CrearListaVacia() {
     return NULL;
 }
 
-Nodo *CrearNodo(Tarea tarea);
-Nodo *CrearNodo(Tarea tarea) {
-    // creo un nodo aux y le doy tamaño, para guardar los datos y retornarlo
-    Nodo *nodoNuevo = (Nodo *)malloc(sizeof(Nodo));
-    nodoNuevo->T = tarea;
-    // nodoNuevo->T.TareaID = tarea.TareaID;
-    // doy el tamaño a la variable donde voy a guardar la descripcion
-    // nodoNuevo->T.Descripcion = (char*)malloc(sizeof(char)* strlen(tarea.Descripcion) + 1);
-    // strcpy(nodoNuevo->T.Descripcion, tarea.Descripcion);
-    // nodoNuevo->T.Duracion = tarea.Duracion;
-    nodoNuevo->Siguiente = NULL;
-    return nodoNuevo;
-}
 
-void instertarNodo(Nodo **inicio, Nodo *nodoNuevo);
-void instertarNodo(Nodo **inicio, Nodo *nodoNuevo) {
-    nodoNuevo->Siguiente = *inicio;
-    *inicio = nodoNuevo;
-}
 
-void insertarUltimo(Nodo *inicio, Nodo *nuevoNodo);
-void insertarUltimo(Nodo *inicio, Nodo *nuevoNodo) {
-    // declaro una variable aux que apunte al inicio de la lista para poder recorrerla sin perder la direccion de la variable inicio
-    Nodo *aux = inicio;
-    // el bucle se ejecuta siempre que aux->siguiente sea distinto de null
-    while (aux->Siguiente != NULL) {
-        // como es != de null, guardo en aux la siguiente direccion
-        aux = aux->Siguiente;
-    }
-    // ahora que salio del bucle y aux->siguiente apunta a null(porque llego al ultimo nodo), hago que apunte al nuevo nodo
-    aux->Siguiente = nuevoNodo;
-}
+int main(int argc, char * argv[]){
 
-int generarID(Nodo *inicio);
-int generarID(Nodo *inicio) {
-    if (inicio == NULL) {
-        // Si el inicio es NULL, no hay nodos en la lista, entonces el ID comienza en 1000
-        return 1000;
-    }
-    Nodo *aux = inicio;
-    // Verifico si hay solo un nodo en la lista
-    if (aux->Siguiente == NULL) {
-        return 1000;
-    } else {
-        // Busco el último nodo en la lista
-        // falta ver cual es el mayor id
-        while (aux->Siguiente != NULL) {
-            aux = aux->Siguiente;
-        }
-        // El ID del nuevo nodo es el ID del último nodo + 1
-        return aux->T.TareaID + 1;
-    }
-}
+    Nodo * tareasPendientes = CrearListaVacia();
+    printf("\n %p \n", tareasPendientes);
 
-Tarea crearTarea(Nodo *inicio);
-Tarea crearTarea(Nodo *inicio) {
-    char buff[50];  // var aux para guardar temporalmente la descripcion
-    Tarea tNueva;
-    tNueva.TareaID = generarID(inicio);
-    printf("\nIngrese la descripcion: ");
-    fflush(stdin);
-    gets(buff);
-    tNueva.Descripcion = (char *)malloc(sizeof(char) * strlen(buff) + 1);
-    strcpy(tNueva.Descripcion, buff);
-    printf("\nIngrese la duracion: ");
-    scanf("%d", &tNueva.Duracion);
-    return tNueva;
-}
-
-void cargarTarea(Nodo **inicio);
-void cargarTarea(Nodo **inicio) {
-    int opcion;  // variable bandera para salir del bucle
-    do {
-        Tarea tNueva = crearTarea(*inicio);
-        Nodo *nodoNuevo = CrearNodo(tNueva);
-        //Verifico si inicio es null
-        if(*inicio == NULL){
-            //si es null apunto inicio al nodo nuevo
-            *inicio = nodoNuevo;
-        }else{
-            //si no es null agrego la tarea al ultimo
-            insertarUltimo(*inicio, nodoNuevo);
-        }
-
-        printf("\nDesea cargar otra tarea? Ingrese 1 o 2");
-        printf("\n1-SI");
-        printf("\n2-NO");
-        scanf("%d", &opcion);
-    } while (opcion != 2);
-}
-
-void mostrarTareas(Nodo *inicio);
-void mostrarTareas(Nodo *inicio) {
-    if(inicio == NULL){
-        printf("\nNo hay tareas para mostrar\n");
-        return;
-    }
-    Nodo *aux = inicio;
-    while (aux->Siguiente) {
-        printf("\n-Tarea ID: %d", aux->T.TareaID);
-        aux = aux->Siguiente;
-    }
-}
-
-int main() {
-    Nodo *tareasPendientes = CrearListaVacia();
-    Nodo *tareasRealizadas = CrearListaVacia();
-    cargarTarea(&tareasPendientes);
-    mostrarTareas(tareasPendientes);
     return 0;
 }
