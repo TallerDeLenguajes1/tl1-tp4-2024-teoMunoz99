77c085050b65918345e44a44970d773ea88c796c
Tadeo Munoz <tadeomunoz09@hotmail.com>
2024-05-02 23:53:05 -0300

    agregue una funcion que crea una tarea y la retorna

--- punto2.c
+++ punto2.c
@@ -20,12 +20,27 @@ Nodo *CrearListaVacia() {
     return NULL;
 }
 
-
+//--Funcion que devuelve una tarea
+Tarea crearTarea();
+Tarea crearTarea(){
+    char buff[50];
+    Tarea aux;
+    printf("\nIngrese la descripcion: ");
+    fflush(stdin);
+    gets(buff);
+    aux.Descripcion = (char *)malloc(sizeof(char) * strlen(buff) + 1);
+    strcpy(aux.Descripcion,buff);
+    aux.TareaID = 1;//funcion para generar un id
+    printf("\nIngrese la duracion: ");
+    scanf("%d", &aux.Duracion);//validar que sea entre 10 y 100
+    return aux;
+}
 
 int main(int argc, char * argv[]){
 
     Nodo * tareasPendientes = CrearListaVacia();
-    printf("\n %p \n", tareasPendientes);
+    Tarea aux = crearTarea();
+    printf("\n %s \n", aux.Descripcion);
 
     return 0;
 }
